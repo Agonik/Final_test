da4e31e17c77c6390efae95fe9db66613da64f14
Aleksey <100375680+Agonik@users.noreply.github.com>
2022-08-06 23:17:33 +0300

    Update conftest.py

--- final/conftest.py
+++ final/conftest.py
@@ -14,17 +14,3 @@ def testing_driver():
 
    driver.quit()
 
-
-@pytest.fixture(scope="session")
-def backdrop_testing_driver():
-   """ Фикстура для запуска браузера в фоновом режиме """
-   options = webdriver.ChromeOptions()
-   options.add_argument('--headless')
-   options.add_argument('disable-infobars')
-   driver = webdriver.Chrome(path_driver, options=options)
-
-
-   yield driver
-
-   driver.quit()
-
